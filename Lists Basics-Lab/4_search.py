a891ec58cb2d113d726182d4debc6fbede655376
Kristiqna <123325312+kristiqna-andonova@users.noreply.github.com>
2023-04-13 21:40:35 +0300

    Create 4_search.py

---
+++ Lists Basics-Lab/4_search.py
@@ -0,0 +1,15 @@
+lines = int(input())
+word = input()
+
+strings = []
+strings_including_word = []
+
+for _ in range(lines):
+    current_word = input()
+    strings.append(current_word)
+
+    if word in current_word:
+        strings_including_word.append(current_word)
+
+print(strings)
+print(strings_including_word)

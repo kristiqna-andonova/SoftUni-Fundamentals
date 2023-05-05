6f636bbbaae8bf72784a520276bd4f5be85a4821
Kristiqna <123325312+kristiqna-andonova@users.noreply.github.com>
2023-05-05 23:21:10 +0300

    Update 8_palindrome_integers.py

--- Functions-Exercise/8_palindrome_integers.py
+++ Functions-Exercise/8_palindrome_integers.py
@@ -1,12 +1,12 @@
-numbers = input().split(", ")
+number = input().split(", ")
 
 
-def is_palindrome(number):
-    for n in number:
+def is_palindrome(numbers):
+    for n in numbers:
         if n == n[::-1]:
             print("True")
         else:
             print("False")
 
 
-is_palindrome(numbers)
+is_palindrome(number)

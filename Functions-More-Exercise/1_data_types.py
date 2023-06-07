16c1bae719409a768ac66564ceb0d9c5bbf5b0e1
Kristiqna <123325312+kristiqna-andonova@users.noreply.github.com>
2023-06-07 15:25:56 +0300

    Create 1_data_types.py

---
+++ Functions-More-Exercise/1_data_types.py
@@ -0,0 +1,22 @@
+num_type = str(input())
+number = input()
+
+
+def data_type_teller(inputs, num):
+    result = 0
+    if inputs == "int":
+        n = int(num)
+        result = n * 2
+
+    elif inputs == "real":
+        n = float(num)
+        result = f"{n * 1.5:.2f}"
+
+    elif inputs == "string":
+        string = str(num)
+        return f"${string}$"
+
+    return result
+
+
+print(data_type_teller(num_type, number))
